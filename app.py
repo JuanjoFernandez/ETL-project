e4576b1f1463d64a29ecd1c31d336b78d06d9f04
robmir <roberto.miranda.gomez@gmail.com>
2021-06-12 09:13:15 -0500

    flask work

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_costa
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/movie_app")
-
+mongo = PyMongo(app, uri="mongodb://localhost:27017/Movies")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
@@ -17,12 +15,12 @@ def home():
     destination_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", movie=destination_data)
+    return render_template("template/index.html", movie=destination_data)
 
 
 # Route that will trigger the scrape function
 @app.route("/query")
-def scrape():
+def query():
 
     ####### What we want to do)
 
